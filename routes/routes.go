48a48f564c453b87494920e648fc51dca11661f0
Rishabh Singh <rishabhsvats@gmail.com>
2024-07-12 14:20:20 +0530

    creating the basic structure of the application - model, routes and main.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/rishabhsvats/golang-ecommerce/controllers"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/users/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.POST("/users/productview", controllers.SearchProduct())
+	incomingRoutes.POST("/users/search", controllers.SearchProductByQuery())
+
+}
